d6ee107a0b09c854a99875b10da8021f52570638
FSDonnelly <donnelly_frank@yahoo.com>
2020-01-23 19:05:07 -0800

    finish findIndex exercises

--- FindIndex/findIndex-ex1.js
+++ FindIndex/findIndex-ex1.js
@@ -2,12 +2,3 @@ var arr = [3, 4, 6, 2, 1];
 findIndex(arr, function(num, index, array) {
   return num === 6;
 });
-
-function findIndex(arr, callback) {
-  for (var i = 0; i < arr.length; i++) {
-    if (callback(arr[i], i, arr)) {
-      return i;
-    }
-  }
-  return -1;
-}
