d15a96e04badd6c0543899d6eca57698afa2cdf9
FSDonnelly <donnelly_frank@yahoo.com>
2020-02-10 20:28:01 -0800

    example of promise handling errors

--- Stack/promise.js
+++ Stack/promise.js
@@ -5,3 +5,13 @@ let p1 = new Promise(function(res, rej) {
 p1.then(function(arr) {
   console.log(`Promise p1 resolved with data: ${arr}`);
 });
+
+let p2 = new Promise(function(res, rej) {
+  rej("ERROR");
+});
+
+p2.then(function(data) {
+  console.log(`Promise p2 resolved with data: ${data}`);
+}).catch(function(data) {
+  console.log(`Promise p2 rejected with data: ${data}`);
+});
