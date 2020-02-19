0d5c7520503fdace4579752ba2291a83e8c64f01
FSDonnelly <donnelly_frank@yahoo.com>
2020-02-18 20:45:23 -0800

    example of promise-chaining returning data

--- Async/promise-chaining.js
+++ Async/promise-chaining.js
@@ -17,3 +17,19 @@ promise
   .then(data => {
     console.log(`Second random int passed to resolve: ${data}`);
   });
+
+// Promise Chaining returning data
+let dataPromise = new Promise((res, rej) => {
+  res(5);
+});
+
+dataPromise
+  .then(data => {
+    return data * 2;
+  })
+  .then(data => {
+    return data + 20;
+  })
+  .then(data => {
+    console.log(data);
+  });
