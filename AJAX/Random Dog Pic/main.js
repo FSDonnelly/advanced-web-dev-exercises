685bc6ce6b91e871ebe53442e251859ff0ae3b17
FSDonnelly <donnelly_frank@yahoo.com>
2020-02-20 20:12:42 -0800

    add main.js to Random Dog Pic

--- AJAX/Random Dog Pic/main.js
+++ AJAX/Random Dog Pic/main.js
@@ -1,5 +1,17 @@
 let btn = document.querySelector("#btn");
 
+// listen for clicks
 btn.addEventListener("click", () => {
-  alert("CLICKED!");
+  // make the request
+  let XHR = new XMLHttpRequest();
+
+  XHR.onreadystatechange = () => {
+    if (XHR.readyState == 4 && XHR.status == 200) {
+      let data = JSON.parse(XHR.responseText);
+      console.log(data);
+    }
+  };
+
+  XHR.open("GET", "https://dog.ceo/api/breeds/image/random");
+  XHR.send();
 });
