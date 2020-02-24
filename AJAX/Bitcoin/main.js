a97d181e1adf8e83ad2c00b2b450333412ec36b5
FSDonnelly <donnelly_frank@yahoo.com>
2020-02-23 16:25:55 -0800

    started code for  Bitcoin Exercise folder

--- AJAX/Bitcoin/main.js
+++ AJAX/Bitcoin/main.js
@@ -1,1 +1,19 @@
 // https://api.coindesk.com/v1/bpi/currentprice.json
+let btn = document.querySelector("#btn");
+let price = document.querySelector("#price");
+
+// listen for clicks
+btn.addEventListener("click", () => {
+  // make the request
+  let XHR = new XMLHttpRequest();
+
+  XHR.onreadystatechange = () => {
+    if (XHR.readyState == 4 && XHR.status == 200) {
+      let currentRate = JSON.parse(XHR.responseText).bpi.USD.rate_float;
+      price = currentRate;
+    }
+  };
+
+  XHR.open("GET", "https://api.coindesk.com/v1/bpi/currentprice.json");
+  XHR.send();
+});
