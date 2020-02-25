301a2c7f3c31ae70d67da35daa4667f538ab226c
FSDonnelly <donnelly_frank@yahoo.com>
2020-02-24 19:32:23 -0800

    converting Bitcoin from XHR request to Fetch

--- AJAX/Bitcoin/main.js
+++ AJAX/Bitcoin/main.js
@@ -1,19 +1,27 @@
 // https://api.coindesk.com/v1/bpi/currentprice.json
 let btn = document.querySelector("#btn");
 let price = document.querySelector("#price");
-
+let url = "https://api.coindesk.com/v1/bpi/currentprice.json";
 // listen for clicks
 btn.addEventListener("click", () => {
   // make the request
-  let XHR = new XMLHttpRequest();
+  // let XHR = new XMLHttpRequest();
 
-  XHR.onreadystatechange = () => {
-    if (XHR.readyState == 4 && XHR.status == 200) {
-      let currentRate = JSON.parse(XHR.responseText).bpi.USD.rate;
-      price.innerText = "$" + currentRate + " USD";
-    }
-  };
+  // XHR.onreadystatechange = () => {
+  //   if (XHR.readyState == 4 && XHR.status == 200) {
+  //     let currentRate = JSON.parse(XHR.responseText).bpi.USD.rate;
+  //     price.innerText = "$" + currentRate + " USD";
+  //   }
+  // };
 
-  XHR.open("GET", "https://api.coindesk.com/v1/bpi/currentprice.json");
-  XHR.send();
+  // XHR.open("GET", "https://api.coindesk.com/v1/bpi/currentprice.json");
+  // XHR.send();
+
+  // Make fetch request
+  fetch(url).then(response => {
+    console.log(response);
+    return response.json().then(data => {
+      console.log(data);
+    });
+  });
 });
