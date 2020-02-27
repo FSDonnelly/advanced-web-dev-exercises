c3ffaaf04a5bd70667f1d543f89aaf0d66cc10e2
FSDonnelly <donnelly_frank@yahoo.com>
2020-02-26 19:45:36 -0800

    refactored fetch-handling-errors.js using arrow functions

--- AJAX/fetch-handling-errors.js
+++ AJAX/fetch-handling-errors.js
@@ -1,34 +1,35 @@
 let btn1 = document.querySelector("#btn1");
 let btn2 = document.querySelector("#btn2");
-btn2.addEventListener("click", function() {
+
+btn2.addEventListener("click", () => {
   let url = "https://api.github.com/users/FSDonnelly";
   fetch(url)
     .then(handleErrors)
-    .then(function(request) {
+    .then(request => {
       console.log("EVERYTHING IS FINE!");
       console.log(request);
     })
-    .catch(function(error) {
+    .catch(error => {
       console.log(`There was a problem... error: ${error}`);
     });
 });
 
-btn1.addEventListener("click", function() {
+btn1.addEventListener("click", () => {
   let url = "https://api.github.com/users/FSDonnellyhsh";
   fetch(url)
     .then(handleErrors)
-    .then(function(request) {
+    .then(request => {
       console.log("EVERYTHING IS FINE!");
       console.log(request);
     })
-    .catch(function(error) {
+    .catch(error => {
       console.log(`There was a problem... error: ${error}`);
     });
 });
 
-function handleErrors(request) {
+handleErrors = request => {
   if (!request.ok) {
     throw Error(request.status);
   }
   return request;
-}
+};
