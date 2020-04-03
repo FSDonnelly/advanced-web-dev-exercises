fd9f73e652d47bf03be6230ff54ff3182023257d
FSDonnelly <donnelly_frank@yahoo.com>
2020-04-02 19:50:01 -0700

    forEach Array exersizes

--- Jasmine Testing/Arrays/main.js
+++ Jasmine Testing/Arrays/main.js
@@ -30,3 +30,28 @@ doubleValues = arr => {
 
 doubleValues([1, 2, 3]);
 doubleValues([5, 1, 2, 3, 10]);
+
+onlyEvenValues = arr => {};
+
+onlyEvenValues([1, 2, 3]);
+onlyEvenValues([5, 1, 2, 3, 10]);
+
+showFirstAndLast = arr => {};
+
+showFirstAndLast(["colt", "matt", "tim", "udemy"]);
+showFirstAndLast(["hi", "goodbye", "smile"]);
+
+addKeyAndValue = (arr, key, value) => {};
+addKeyAndValue(
+  [{ name: "Elie" }, { name: "Tim" }, { name: "Matt" }, { name: "Colt" }],
+  "title",
+  "instructor"
+);
+
+vowelCount = str => {};
+
+vowelCount("Elie"); // {e:2,i:1};
+vowelCount("Tim"); // {i:1};
+vowelCount("Matt"); // {a:1})
+vowelCount("hmmm"); // {};
+vowelCount("I Am awesome and so are you"); // {i: 1, a: 4, e: 3, o: 3, u: 1};
