6564e8a86f9bab2a1884dcb434ec927d985c89f1
FSDonnelly <donnelly_frank@yahoo.com>
2020-04-09 20:00:45 -0700

    added some & every method examples

--- Jasmine Testing/Arrays/some_every.js
+++ Jasmine Testing/Arrays/some_every.js
@@ -4,3 +4,23 @@ someArr.some((value, index, array) => {
   console.log(value < 2);
   return value < 2;
 });
+
+console.log(someArr);
+function some(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i], i, array)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function hasEvenNumber(arr) {
+  return arr.some((value) => {
+    return value % 2 === 0;
+  });
+}
+console.log(arr);
+
+hasEvenNumber([1, 2, 3, 4]);
+hasEvenNumber([1, 3, 5]);
