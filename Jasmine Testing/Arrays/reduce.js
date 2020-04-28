1d19649a0ff8a9aae4b0debd8d26a984f630441d
FSDonnelly <donnelly_frank@yahoo.com>
2020-04-27 21:54:09 -0700

    add reduce.js for Reduce exercises

--- Jasmine Testing/Arrays/reduce.js
+++ Jasmine Testing/Arrays/reduce.js
@@ -4,6 +4,13 @@ function extractValue(arr, key) {
     return acc;
   }, []);
 }
+var arr = [
+  { name: "Elie" },
+  { name: "Tim" },
+  { name: "Matt" },
+  { name: "Colt" },
+];
+extractValue(arr, "name"); // ['Elie', 'Tim', 'Matt', 'Colt'
 
 function vowelCount(str) {
   var vowels = "aeiou";
@@ -21,6 +28,11 @@ function vowelCount(str) {
       return acc;
     }, {});
 }
+vowelCount("Elie"); // {e:2,i:1};
+vowelCount("Tim"); // {i:1};
+vowelCount("Matt"); // {a:1})
+vowelCount("hmmm"); // {};
+vowelCount("I Am awesome and so are you"); // {i: 1, a: 4, e: 3, o: 3, u: 1};
 
 function addKeyAndValue(arr, key, value) {
   return arr.reduce(function (acc, next, idx) {
@@ -28,6 +40,18 @@ function addKeyAndValue(arr, key, value) {
     return acc;
   }, arr);
 }
+var arr = [
+  { name: "Elie" },
+  { name: "Tim" },
+  { name: "Matt" },
+  { name: "Colt" },
+];
+addKeyAndValue(arr, "title", "Instructor")[ //
+  ({ title: "Instructor", name: "Elie" },
+  { title: "Instructor", name: "Tim" },
+  { title: "Instructor", name: "Matt" },
+  { title: "Instructor", name: "Colt" })
+];
 
 function partition(arr, callback) {
   return arr.reduce(
@@ -42,3 +66,19 @@ function partition(arr, callback) {
     [[], []]
   );
 }
+
+function isEven(val) {
+  return val % 2 === 0;
+}
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+partition(arr, isEven); // [[2,4,6,8], [1,3,5,7]];
+
+function isLongerThanThreeCharacters(val) {
+  return val.length > 3;
+}
+
+var names = ["Elie", "Colt", "Tim", "Matt"];
+
+partition(names, isLongerThanThreeCharacters); // [['Elie', 'Colt', 'Matt'], ['Tim']]
