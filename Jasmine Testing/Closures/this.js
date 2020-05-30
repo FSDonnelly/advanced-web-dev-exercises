f73a593e7430c2133cfe0f123953848d744fb72f
FSDonnelly <donnelly_frank@yahoo.com>
2020-05-29 19:52:25 -0700

    add call example to this.js

--- Jasmine Testing/Closures/this.js
+++ Jasmine Testing/Closures/this.js
@@ -6,9 +6,26 @@ let person = {
   determineContext: function () {
     return this === person;
   },
+  dog: {
+    sayHello: function () {
+      return `Hello ${this.firstName}`;
+    },
+    determineContext: function () {
+      return this === person;
+    },
+  },
 };
 undefined;
 console.log(person.sayHi());
+console.log(person.dog.sayHello());
+console.log(person.dog.sayHello.call(person));
 // ("Hi Ellie");
 console.log(person.determineContext());
 // true;
+
+firstFunction = () => {
+  console.log("Hello");
+};
+
+console.log(firstFunction);
+firstFunction();
